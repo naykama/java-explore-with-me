5998c28897a7f9c9e41bf2efa3b14e443cd6569b
Naykama <martyanova.anastasya2017@yandex.ru>
2024-04-16 17:17:39 +0300

    Создана структура проекта

---
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/StatsClient.java
@@ -0,0 +1,8 @@
+package ru.practicum.ewm.client.stats;
+
+import org.springframework.web.client.RestTemplate;
+
+public class StatsClient {
+    RestTemplate restTemplate;
+    // 2 метода для отправки запросов в stats-server
+}
